6171f8668df41836ceab4ff26f5bb74c1a1c3e8e
Jakub Skokan <jakub.skokan@havefun.cz>
2017-02-10 19:04:58 +0100

    Add template request_resolve_user_registration_denied

---
+++ request_resolve_user_registration_denied/meta.rb
@@ -0,0 +1,14 @@
+template :request_action_role_type_state do
+  label        'Request registration denied (user)'
+  from         'podpora@vpsfree.cz'
+  reply_to     'podpora@vpsfree.cz'
+  return_path  'podpora@vpsfree.cz'
+
+  lang :cs do
+    subject    'Re: [vpsFree.cz] Přihláška přijata'
+  end
+
+  lang :en do
+    subject    'Re: [vpsFree.cz] Registration accepted'
+  end
+end

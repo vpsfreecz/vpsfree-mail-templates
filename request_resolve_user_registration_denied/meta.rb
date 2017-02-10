68226ec819aec9962075ab8023343d7824d35c98
Jakub Skokan <jakub.skokan@havefun.cz>
2017-02-10 19:15:05 +0100

    request_resolve_user_registration_denied: fix template id

--- request_resolve_user_registration_denied/meta.rb
+++ request_resolve_user_registration_denied/meta.rb
@@ -1,4 +1,4 @@
-template :request_action_role_type_state do
+template :request_resolve_role_type_state do
   label        'Request registration denied (user)'
   from         'podpora@vpsfree.cz'
   reply_to     'podpora@vpsfree.cz'

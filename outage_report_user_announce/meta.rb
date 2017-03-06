d46b878716e1c586c3bb9a145ff87429df4a01be
Jakub Skokan <jakub.skokan@havefun.cz>
2017-03-06 15:12:04 +0100

    Add templates for outage reports

---
+++ outage_report_user_announce/meta.rb
@@ -0,0 +1,14 @@
+template :outage_report_role_event do
+  label        'User outage report announcement'
+  from         'podpora@vpsfree.cz'
+  reply_to     'podpora@vpsfree.cz'
+  return_path  'podpora@vpsfree.cz'
+
+  lang :en do
+    subject    "[vpsFree.cz] <%= @o.planned ? 'Planned' : 'Unplanned' %> outage - <%= @o.outage_entities.map { |e| e.real_name }.join(', ') %> - <%= @o.begins_at.localtime.strftime('%Y-%m-%d %H:%M %Z') %>"
+  end
+
+  lang :cs do
+    subject    "[vpsFree.cz] <%= @o.planned ? 'Plánovaný' : 'Neplánovaný' %> výpadek - <%= @o.outage_entities.map { |e| e.real_name }.join(', ') %> - <%= @o.begins_at.localtime.strftime('%Y-%m-%d %H:%M %Z') %>"
+  end
+end

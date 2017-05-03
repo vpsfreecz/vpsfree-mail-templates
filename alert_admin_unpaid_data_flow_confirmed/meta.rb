095c4b5a530229504b68486b29e3d98f6b4815fb
Jakub Skokan <jakub.skokan@havefun.cz>
2017-05-03 16:55:53 +0200

    Rename alert templates to contain event state

--- alert_admin_unpaid_data_flow_confirmed/meta.rb
+++ alert_admin_unpaid_data_flow_confirmed/meta.rb
@@ -1,4 +1,4 @@
-template :alert_role_event do
+template :alert_role_event_state do
   label        'Unpaid data flow alert'
   from         'vpsadmin@vpsfree.cz'
   subject      'Unpaid user data flow: VPS <%= @event.object.id %> on <%= @event.object.node.domain_name %>'
--- alert_user_diskspace_confirmed_hypervisor/meta.rb
+++ alert_user_diskspace_confirmed_hypervisor/meta.rb
@@ -1,18 +1,18 @@
-template :alert_role_diskspace_pool do
-  label 'Alert on low diskspace of NAS datasets'
+template :alert_role_diskspace_state_pool do
+  label 'Alert on low diskspace of VPS datasets'
   user_visibility true
 
   lang :cs do
     from        'podpora@vpsfree.cz'
     reply_to    'podpora@vpsfree.cz'
     return_path 'podpora@vpsfree.cz'
-    subject     '[vpsFree.cz] NAS <%= @ds.full_name %>: zabráno více než 90 % disku'
+    subject     '[vpsFree.cz] VPS #<%= @vps.id %>: zabráno více než 90 % disku'
   end
 
   lang :en do
     from        'support@vpsfree.org'
     reply_to    'support@vpsfree.org'
     return_path 'support@vpsfree.org'
-    subject     '[vpsFree.cz] NAS <%= @ds.full_name %>: used more than 90 % of disk space'
+    subject     '[vpsFree.cz] VPS #<%= @vps.id %>: used more than 90 % of disk space'
   end
 end

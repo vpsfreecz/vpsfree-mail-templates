620adde8bf2d406f32f39257a85a3fab6207a056
Jakub Skokan <jakub.skokan@havefun.cz>
2018-04-29 11:20:19 +0200

    Outage reports: unplanned -> outage, planned -> maintenance

--- outage_report_generic_update/meta.rb
+++ outage_report_generic_update/meta.rb
@@ -6,6 +6,6 @@ template :outage_report_role_event do
   user_visibility false
 
   lang :en do
-    subject    "Re: <%= @o.planned ? 'Plánovaný výpadek / Planned outage' : 'Neplánovaný výpadek / Unplanned outage' %> - <%= @o.outage_entities.map { |e| e.real_name }.join(', ') %> - <%= @o.begins_at.localtime.strftime('%Y-%m-%d %H:%M %Z') %>"
+    subject    "Re: <%= @o.planned ? 'Odstávka / Maintenance' : 'Výpadek / Outage' %> - <%= @o.outage_entities.map { |e| e.real_name }.join(', ') %> - <%= @o.begins_at.localtime.strftime('%Y-%m-%d %H:%M %Z') %>"
   end
 end
--- outage_report_user_announce/meta.rb
+++ outage_report_user_announce/meta.rb
@@ -5,13 +5,13 @@ template :outage_report_role_event do
     from        'support@vpsfree.org'
     reply_to    'support@vpsfree.org'
     return_path 'support@vpsfree.org'
-    subject     "[vpsFree.cz] <%= @o.planned ? 'Planned' : 'Unplanned' %> outage - <%= @o.outage_entities.map { |e| e.real_name }.join(', ') %> - <%= @o.begins_at.localtime.strftime('%Y-%m-%d %H:%M %Z') %>"
+    subject     "[vpsFree.cz] <%= @o.planned ? 'Maintenance' : 'Outage' %> - <%= @o.outage_entities.map { |e| e.real_name }.join(', ') %> - <%= @o.begins_at.localtime.strftime('%Y-%m-%d %H:%M %Z') %>"
   end
 
   lang :cs do
     from        'podpora@vpsfree.cz'
     reply_to    'podpora@vpsfree.cz'
     return_path 'podpora@vpsfree.cz'
-    subject     "[vpsFree.cz] <%= @o.planned ? 'Plánovaný' : 'Neplánovaný' %> výpadek - <%= @o.outage_entities.map { |e| e.real_name }.join(', ') %> - <%= @o.begins_at.localtime.strftime('%Y-%m-%d %H:%M %Z') %>"
+    subject     "[vpsFree.cz] <%= @o.planned ? 'Odstávka' : 'Výpadek' %> - <%= @o.outage_entities.map { |e| e.real_name }.join(', ') %> - <%= @o.begins_at.localtime.strftime('%Y-%m-%d %H:%M %Z') %>"
   end
 end

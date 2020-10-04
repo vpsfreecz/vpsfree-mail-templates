bd790b7bf830c9eb778d80c4cb75d7f5fab52c13
Jakub Skokan <jakub.skokan@havefun.cz>
2020-10-04 22:17:52 +0200

    Add VPS OOM reports

---
+++ vps_oom_report/meta.rb
@@ -0,0 +1,17 @@
+template do
+  label 'VPS OOM report'
+
+  lang :cs do
+    from        'podpora@vpsfree.cz'
+    reply_to    'podpora@vpsfree.cz'
+    return_path 'podpora@vpsfree.cz'
+    subject     '[vpsFree.cz] Ve VPS #<%= @vps.id %> dochází paměť'
+  end
+
+  lang :en do
+    from        'support@vpsfree.org'
+    reply_to    'support@vpsfree.org'
+    return_path 'support@vpsfree.org'
+    subject     '[vpsFree.cz] VPS #<%= @vps.id %> is running out of memory'
+  end
+end

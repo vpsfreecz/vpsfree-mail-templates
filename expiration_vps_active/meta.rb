acb9c815b257ac4a22bbb91d4756fc64f711e01d
Jakub Skokan <jakub.skokan@havefun.cz>
2022-02-05 19:49:40 +0100

    expiration_vps_active: warn day before suspension

--- expiration_vps_active/meta.rb
+++ expiration_vps_active/meta.rb
@@ -5,13 +5,13 @@ template :expiration_warning do
     from        'podpora@vpsfree.cz'
     reply_to    'podpora@vpsfree.cz'
     return_path 'podpora@vpsfree.cz'
-    subject     '[vpsFree.cz] Blíží se konec životnosti #<%= @vps.id %> <%= @vps.hostname %>'
+    subject     '[vpsFree.cz] <% if @expires_in_a_day %>VPS #<%= @vps.id %> <%= @vps.hostname %> bude pozastaveno<% else %>Blíží se konec životnosti #<%= @vps.id %> <%= @vps.hostname %><% end %>'
   end
 
   lang :en do
     from        'support@vpsfree.org'
     reply_to    'support@vpsfree.org'
     return_path 'support@vpsfree.org'
-    subject     '[vpsFree.cz] VPS #<%= @vps.id %> <%= @vps.hostname %> is nearing its end-of-life'
+    subject     '[vpsFree.cz] <% if @expires_in_a_day %>VPS #<%= @vps.id %> <%= @vps.hostname %> is going to be suspended<% else %>VPS #<%= @vps.id %> <%= @vps.hostname %> is nearing its end-of-life<% end %>'
   end
 end

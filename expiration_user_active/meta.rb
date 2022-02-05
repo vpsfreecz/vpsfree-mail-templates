4b14e31bda0dbe823c8b61583c83405f48cdb128
Jakub Skokan <jakub.skokan@havefun.cz>
2022-02-05 19:49:06 +0100

    expiration_user_active: warn day before suspension

--- expiration_user_active/meta.rb
+++ expiration_user_active/meta.rb
@@ -5,13 +5,13 @@ template :expiration_warning do
     from        'podpora@vpsfree.cz'
     reply_to    'podpora@vpsfree.cz'
     return_path 'podpora@vpsfree.cz'
-    subject     '[vpsFree.cz] Platba členského příspěvku - <%= @object.login %>'
+    subject     '[vpsFree.cz] <% if @expired_days_ago.between?(13, 14) %>Členství <%= @user.login %> bude pozastaveno<% else %>Platba členského příspěvku - <%= @user.login %><% end %>'
   end
 
   lang :en do
     from        'support@vpsfree.org'
     reply_to    'support@vpsfree.org'
     return_path 'support@vpsfree.org'
-    subject     '[vpsFree.cz] Payment of membership fee - <%= @object.login %>'
+    subject     '[vpsFree.cz] <% if @expired_days_ago.between?(13, 14) %>Membership <%= @user.login %> is going to be suspended<% else %>Payment of membership fee - <%= @user.login %><% end %>'
   end
 end

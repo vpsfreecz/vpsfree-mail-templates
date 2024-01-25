ecf804d5e3501f9893e69509240c3381b25ba051
Jakub Skokan <jakub.skokan@havefun.cz>
2024-01-25 10:19:19 +0100

    outage_report_*: use outage and impact type

--- outage_report_generic_update/meta.rb
+++ outage_report_generic_update/meta.rb
@@ -6,6 +6,6 @@ template :outage_report_role_event do
   user_visibility false
 
   lang :en do
-    subject    "Re: <%= @o.planned ? 'Odstávka / Maintenance' : 'Výpadek / Outage' %> - <%= @o.outage_entities.map { |e| e.real_name }.join(', ') %> - <%= @o.begins_at.localtime.strftime('%Y-%m-%d %H:%M %Z') %>"
+    subject    "Re: <%= @o.maintenance? ? 'Odstávka / Maintenance' : 'Výpadek / Outage' %> - <%= @o.outage_entities.map { |e| e.real_name }.join(', ') %> - <%= @o.begins_at.localtime.strftime('%Y-%m-%d %H:%M %Z') %>"
   end
 end

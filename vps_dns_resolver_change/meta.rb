aaee2b067f0b553a6360e4d68b66bd0cc59baeaa
Jakub Skokan <jakub.skokan@havefun.cz>
2024-04-16 19:39:01 +0200

    Add vps_dns_resolver_change

---
+++ vps_dns_resolver_change/meta.rb
@@ -0,0 +1,17 @@
+template do
+  label 'VPS DNS resolver changed'
+
+  lang :cs do
+    from        'podpora@vpsfree.cz'
+    reply_to    'podpora@vpsfree.cz'
+    return_path 'podpora@vpsfree.cz'
+    subject     '[vpsFree.cz] VPS #<%= @vps.id %>: změna DNS resolveru'
+  end
+
+  lang :en do
+    from        'support@vpsfree.org'
+    reply_to    'support@vpsfree.org'
+    return_path 'support@vpsfree.org'
+    subject     '[vpsFree.cz] VPS #<%= @vps.id %>: DNS resolver changed'
+  end
+end

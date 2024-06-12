8fc9315abb3f1912651e011172fdf0ae9a8e1162
Jakub Skokan <jakub.skokan@havefun.cz>
2024-06-12 19:48:59 +0200

    Add user_failed_logins

---
+++ user_failed_logins/meta.rb
@@ -0,0 +1,17 @@
+template do
+  label 'Failed logins'
+
+  lang :cs do
+    from        'podpora@vpsfree.cz'
+    reply_to    'podpora@vpsfree.cz'
+    return_path 'podpora@vpsfree.cz'
+    subject     '[vpsFree.cz] Pokus o přihlášení do vpsAdminu'
+  end
+
+  lang :en do
+    from        'support@vpsfree.org'
+    reply_to    'support@vpsfree.org'
+    return_path 'support@vpsfree.org'
+    subject     '[vpsFree.cz] vpsAdmin login attempts detected'
+  end
+end

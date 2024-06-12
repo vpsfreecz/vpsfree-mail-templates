17add7de0370a643659dd2d4040b8b0c2e5680cd
Jakub Skokan <jakub.skokan@havefun.cz>
2024-06-12 19:48:51 +0200

    Add user_new_token

---
+++ user_new_token/meta.rb
@@ -0,0 +1,17 @@
+template do
+  label 'New login token created'
+
+  lang :cs do
+    from        'podpora@vpsfree.cz'
+    reply_to    'podpora@vpsfree.cz'
+    return_path 'podpora@vpsfree.cz'
+    subject     '[vpsFree.cz] Vytvořen nový token do vpsAdminu'
+  end
+
+  lang :en do
+    from        'support@vpsfree.org'
+    reply_to    'support@vpsfree.org'
+    return_path 'support@vpsfree.org'
+    subject     '[vpsFree.cz] New vpsAdmin access token'
+  end
+end

7da522e060fc18d5426e1dd6cd305b6847faf5ed
Jakub Skokan <jakub.skokan@havefun.cz>
2026-06-07 09:50:50 +0200

    templates: render timestamps in user time zone
    Use vpsAdmin mail template helpers for user-visible timestamps instead of
    Ruby localtime formatting. This lets uploaded templates render dates and
    times in the recipient user's configured time zone.
    
    Keep pure business dates, such as membership validity, as date-only values
    where they are not tied to a specific moment in time.

--- daily_report/meta.rb
+++ daily_report/meta.rb
@@ -1,5 +1,5 @@
 template do
   label        'Daily report for admins'
   from         'vpsadmin@vpsfree.cz'
-  subject      'vpsAdmin daily report <%= @date[:start].strftime("%d/%m/%Y") %>'
+  subject      'vpsAdmin daily report <%= local_date(@date[:start], "%d/%m/%Y") %>'
 end

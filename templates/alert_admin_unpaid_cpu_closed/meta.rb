1e79609d23d44c820b221669d162eabb827bc24f
Jakub Skokan <jakub.skokan@havefun.cz>
2026-07-30 23:32:40 +0200

    monitoring: rename alert templates by event
    Rename monitoring alert template directories and IDs away from the old
    admin/user role naming. The new names match concrete event types defined by
    vpsAdmin configuration.

--- templates/alert_admin_unpaid_cpu_closed/meta.rb
+++
@@ -1,7 +0,0 @@
-template :alert_role_event_state do
-  label 'Unpaid CPU overuse alert closed'
-
-  protocol :email do
-    from        'vpsadmin@vpsfree.cz'
-  end
-end
---
+++ templates/alert_dns_secondary_transfer_failure_closed/meta.rb
@@ -0,0 +1,19 @@
+template :alert_dns_secondary_transfer_failure_closed do
+  label 'DNS secondary transfer failure resolved'
+  user_visibility true
+
+  protocol :email do
+
+    lang :cs do
+      from        'podpora@vpsfree.cz'
+      reply_to    'podpora@vpsfree.cz'
+      return_path 'podpora@vpsfree.cz'
+    end
+
+    lang :en do
+      from        'support@vpsfree.org'
+      reply_to    'support@vpsfree.org'
+      return_path 'support@vpsfree.org'
+    end
+  end
+end
